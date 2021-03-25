7e8acbdc5d8599400c0f45d3c2dec5e7f096888a
austingandy <austin.gandy@gmail.com>
2021-03-25 13:23:31 -0400

    mostly finished introductory tutorial

--- hello-world.flow.ts
+++ hello-world.flow.ts
@@ -1,12 +1,12 @@
 import { collections, interfaces, registers } from 'flow/modules';
 
-// Implementation for derivation flow.yaml#/collections/greetings/derivation.
+// Implementation for derivation hello-world.flow.yaml#/collections/greetings/derivation.
 export class Greetings implements interfaces.Greetings {
     sayHelloPublish(
         source: collections.People,
-        _register: registers.Greetings,
-        _previous: registers.Greetings,
+        register: registers.Greetings,
+        previous: registers.Greetings,
     ): collections.Greetings[] {
-        return [{ id: source.id, greeting: `Hello ${source.name}!` }];
+        return [{id: source.id, greeting:`Hello ${source.name}!`}];
     }
 }
